c698bd460216562c9a94ea5fc051e6f5282cea57
Toaster6746 <69042510+Toaster6746@users.noreply.github.com>
2024-08-26 14:36:49 -0400

    Update PersonalInfo.cs

--- Module 1/PersonalInfo.cs
+++ Module 1/PersonalInfo.cs
@@ -1,16 +1,1 @@
-Console.WriteLine("Lucinda Potter");
-Console.WriteLine("6/24/1992");
-Console.WriteLine("work 000-000-0101");
-Console.WriteLine("cell 000-000-0189");
-Console.WriteLine("John Smith");
-Console.WriteLine("8/15/1978");
-Console.WriteLine("work 000-111-0101");
-Console.WriteLine("cell 000-111-0189");
-Console.WriteLine("Chris Johnson");
-Console.WriteLine("12/01/1987");
-Console.WriteLine("work 000-222-0101");
-Console.WriteLine("cell 000-222-0189");
-Console.WriteLine("Robert Hall");
-Console.WriteLine("2/27/1949");
-Console.WriteLine("work 000-444-0101");
-Console.WriteLine("cell 000-444-0189");
+
