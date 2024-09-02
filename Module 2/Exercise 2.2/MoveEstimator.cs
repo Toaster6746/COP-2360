20eaf356ec6b3eaee5a705a44897c3f843ca4b81
Toaster6746 <69042510+Toaster6746@users.noreply.github.com>
2024-09-02 15:13:59 -0400

    Update MoveEstimator.cs

--- Module 2/Exercise 2.2/MoveEstimator.cs
+++ Module 2/Exercise 2.2/MoveEstimator.cs
@@ -1,1 +1,7 @@
-
+string hours = Console.ReadLine();
+string hoursNumberPart = hours.Remove(hours.IndexOf(' '));
+int hoursNum = int.Parse(hoursNumberPart);
+string miles = Console.ReadLine();
+string milesNumberPart = miles.Remove(miles.IndexOf(' '));
+int milesNum = int.Parse(milesNumberPart);
+Console.WriteLine("$" + (200 + (milesNum * 2) + (hoursNum * 150)));
