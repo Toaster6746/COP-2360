aab9da2f10d2042afc5ae85222158e0996b29ba8
Toaster6746 <69042510+Toaster6746@users.noreply.github.com>
2024-09-16 11:47:14 -0400

    Create Perfect.cs

---
+++ Module 4/Exercise 4.1/Perfect.cs
@@ -0,0 +1,21 @@
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 6)
+{
+    Console.WriteLine("Perfect Number");
+}
+else if (num == 28)
+{
+    Console.WriteLine("Perfect Number");
+}
+else if (num == 496)
+{
+    Console.WriteLine("Perfect Number");
+}
+else if (num == 8128)
+{
+    Console.WriteLine("Perfect Number");
+}
+else
+{
+    Console.WriteLine("Not A Perfect Number");
+}
